ef0644a469d6130aed7c199bfece1be15e146098
Lam Thanh Hoai <lamthanhhoai1990@gmail.com>
2018-06-26 22:47:30 +0700

    update Category ImageBackground add text

--- components/Main/Shop/Home/Category.js
+++ components/Main/Shop/Home/Category.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Dimensions, Image } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, Image, ImageBackground} from 'react-native';
 import Swiper from 'react-native-swiper';
 
 import slide1 from '../../../../media/temp/maxi.jpg';
@@ -11,17 +11,25 @@ const { width } = Dimensions.get('window');
 
 export class Category extends Component {
     render() {
-        const { wapper, textTitle, banerStyle } = styles;
+        const { wapper, textTitle, banerStyle, cateTitle } = styles;
         return (
             <View style={wapper}>
                 <View>
                     <Text style={textTitle}>LIST OF CATEGORY</Text>
                 </View>
                 <Swiper showsButtons={true}>
-                    <Image source={slide1} style={banerStyle}/>
-                    <Image source={slide2} style={banerStyle}/>
-                    <Image source={slide3} style={banerStyle}/>
-                    <Image source={slide4} style={banerStyle}/>
+                    <ImageBackground  source={slide1} style={banerStyle}>
+                        <Text style={cateTitle}>Maxi Dress</Text>
+                    </ImageBackground >
+                    <ImageBackground  source={slide2} style={banerStyle}>
+                        <Text style={cateTitle}>Maxi Dress</Text>
+                    </ImageBackground >
+                    <ImageBackground  source={slide3} style={banerStyle}>
+                        <Text style={cateTitle}>Maxi Dress</Text>
+                    </ImageBackground >
+                    <ImageBackground  source={slide4} style={banerStyle}>
+                        <Text style={cateTitle}>Maxi Dress</Text>
+                    </ImageBackground >
                 </Swiper>
             </View>
         )
@@ -30,7 +38,7 @@ export class Category extends Component {
 
 const banerWidth = width - 40;
 const banerHeight = width / 2;
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     wapper: {
         padding: 10,
         margin: 10,
@@ -40,10 +48,17 @@ const styles = StyleSheet.create ({
     banerStyle: {
         width: banerWidth,
         height: banerHeight,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     textTitle: {
         color: '#D3D3D3',
         fontSize: 20,
         marginBottom: 10,
+    },
+    cateTitle: {
+        fontSize: 20,
+        fontFamily: 'Avenir',
+        color: '#9A9A9A',
     }
 })
